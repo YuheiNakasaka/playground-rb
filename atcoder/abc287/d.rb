0d9f2a66ecb2bdcef031abffca0dbb727bbe2e16
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-01-29 10:48:37 +0900

    abc287_dを解説読んで解いた

--- atcoder/abc287/d.rb
+++ atcoder/abc287/d.rb
@@ -1,15 +1,62 @@
 # https://atcoder.jp/contests/abc287/tasks/abc287_d
+
+# 実験1
+# |S| = 3
+# |T| = 2
+# x = 0, |T| - x = 2 => ?c
+# x = 1, |T| - x = 1 => ac
+# x = 2, |T| - x = 0 => a?
+# a?c
+# b?
+#
+# 実験2
+# |S| = 7
+# |T| = 4
+# x = 2, |T| - x = 2 => abef
+# x = 1, |T| - x = 3 => adef
+# abc?def
+# bc?r
+#
+# SとTがマッチするにはSi==Tiである必要がある => (1)
+# S'とTがマッチするということは「Sの先頭x文字とTの先頭x文字がマッチ」しかつ「Sの末尾|T|-x文字とTの末尾|T|-x文字がマッチ」するということ => (2)
+# つまりSとTの先頭がどこまでマッチするか？とSとTの末尾がどこまでマッチするか？をそれぞれ求めればよい
+# あとはその結果を0~|T|までの全てのxに対して比較して答えを出す
+
 S = gets.chomp
 T = gets.chomp
 sl = S.length
 tl = T.length
 
-s2 = []
-(tl + 1).times do |x|
-  s2 << S[0, x] + S[sl - tl + x, tl - x]
+pre = Array.new(sl + 1, false)
+suf = Array.new(sl + 1, false)
+
+pre[0] = true
+sl.times do |i|
+  break if i > tl
+  if S[i] == T[i] || S[i] == "?" || T[i] == "?"
+    pre[i + 1] = true
+  else
+    break
+  end
 end
 
-p(s2)
-T.chars do |c|
-  p(c)
+S.reverse!
+T.reverse!
+
+suf[0] = true
+sl.times do |i|
+  break if i > tl
+  if S[i] == T[i] || S[i] == "?" || T[i] == "?"
+    suf[i + 1] = true
+  else
+    break
+  end
+end
+
+(tl + 1).times do |x|
+  if pre[x] && suf[tl - x]
+    puts("Yes")
+  else
+    puts("No")
+  end
 end
