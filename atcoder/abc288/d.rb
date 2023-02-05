8f27c40ce622903b05910c907b9999fcfb8f2e67
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-02-05 09:47:58 +0900

    abc288 dを解説見てといた

--- atcoder/abc288/d.rb
+++ atcoder/abc288/d.rb
@@ -3,21 +3,37 @@
 #
 # 普通にTLEになるな...
 #
-# わからん。解説。
+# わからん。解説見て再度解いた。
 # https://atcoder.jp/contests/abc288/editorial/5664
-
-require "set"
 
 N, K = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 q = gets.to_i
-q.times do |qq|
+
+cum = Array.new(K) { Array.new(N + 1, 0) }
+K.times do |j|
+  N.times do |i|
+    cum[j][i + 1] = cum[j][i]
+    if i % K == j
+      cum[j][i + 1] += a[i]
+    end
+  end
+end
+
+q.times do
   l, r = gets.split.map(&:to_i)
-  x = a[l - 1..r - 1]
-  cum = Array.new(K, 0)
-  0.upto(x.size - 1) do |i|
-    cum[i % K] += x[i]
+  init = cum[0][r] - cum[0][l - 1]
+  flag = true
+  1.upto(K - 1) do |j|
+    flag = init == (cum[j][r] - cum[j][l - 1])
+    if !flag
+      break
+    end
   end
 
-  Set.new(cum).size == 1 ? puts("Yes") : puts("No")
+  if flag
+    puts("Yes")
+  else
+    puts("No")
+  end
 end
