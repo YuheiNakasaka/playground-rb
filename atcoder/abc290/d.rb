14bc796d4b7b9bf7593619fc888f1e12f83c8f85
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-02-20 11:48:23 +0900

    abc290のdを解説見てといた

--- atcoder/abc290/d.rb
+++ atcoder/abc290/d.rb
@@ -1,23 +1,15 @@
+# https://atcoder.jp/contests/abc290/tasks/abc290_d
+#
+# 解説。数学だ...
+# https://atcoder.jp/contests/abc290/editorial/5767
+
 T = gets.to_i
 ans = []
 T.times do |i|
   n, d, k = gets.split.map(&:to_i)
-  mark = Array.new(n - 1, false)
-  init = 0
-  kcnt = 0
-
-  while kcnt < k
-    x = (init + d) % n
-    if !mark[x]
-      mark[x] = true
-      kcnt += 1
-    end
-
-    if kcnt == k
-      ans << x
-      mark = Array.new(n - 1, false)
-    end
-  end
+  k -= 1
+  a = n / n.gcd(d)
+  ans << d * k % n + k / a
 end
 
 ans.each do |a|
