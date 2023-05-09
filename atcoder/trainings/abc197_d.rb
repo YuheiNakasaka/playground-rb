b65ad05b9d3a00f9684924e132b01f7823d89e9a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-05-09 18:28:59 +0900

    abc197 d 2

--- atcoder/trainings/abc197_d.rb
+++ atcoder/trainings/abc197_d.rb
@@ -12,17 +12,30 @@
 N = gets.to_i
 x0, y0 = gets.split.map(&:to_i)
 xn_2, yn_2 = gets.split.map(&:to_i)
-# 中点
-ox = (xn_2 + x0) / 2.0
-oy = (yn_2 + y0) / 2.0
-# 中点を原点に移動した時のp0の座標
-xo2 = x0 - ox
-yo2 = y0 - oy
-# p1は360/Nだけ移動した点
-rad = (360 / N.to_f) * (Math::PI / 180)
-cos = Math.cos(rad)
-sin = Math.sin(rad)
-# 回転行列でp1の座標を求める
-x2 = xo2 * cos - yo2 * sin + ox
-y2 = xo2 * sin + yo2 * cos + oy
-puts("#{x2} #{y2}")
+
+# 回転行列を用いた回答
+# # 中点
+# ox = (xn_2 + x0) / 2.0
+# oy = (yn_2 + y0) / 2.0
+# # 中点を原点に移動した時のp0の座標
+# xo2 = x0 - ox
+# yo2 = y0 - oy
+# # p1は360/Nだけ移動した点
+# rad = (360 / N.to_f) * (Math::PI / 180)
+# cos = Math.cos(rad)
+# sin = Math.sin(rad)
+# # 回転行列でp1の座標を求める
+# x2 = xo2 * cos - yo2 * sin + ox
+# y2 = xo2 * sin + yo2 * cos + oy
+# puts("#{x2} #{y2}")
+
+# 複素数を用いた回答
+# 中点の複素数表現
+m = Complex((xn_2 + x0) / 2.0, (yn_2 + y0) / 2.0)
+# aの複素数表現
+a = Complex(x0 - m.real, y0 - m.imag)
+# 長さ1、角度360/N=2pi/Nの複素数表現
+r = Complex.polar(1, Math::PI * 2.0 / N)
+# θ回転させたいとき、回転させたい複素平面の座標に偏角θの単位ベクトルをかければよい
+res = m + a * r
+puts("#{res.real} #{res.imag}")
