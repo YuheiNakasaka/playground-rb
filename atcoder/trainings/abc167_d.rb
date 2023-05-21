a33aa49060504579a3d50bbd4f213fc00502243e
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-05-21 10:07:56 +0900

    ダブリング

--- atcoder/trainings/abc167_d.rb
+++ atcoder/trainings/abc167_d.rb
@@ -15,36 +15,63 @@
 # 周期性に注目することとmodを使うこともわかったがシュミレートしきれなかった。
 # 以下は解説を読んでから再度解いてみた。
 
+# N, $K = gets.split.map(&:to_i)
+# A = gets.split.map(&:to_i)
+# A.unshift(1)
+# record = []
+# seen = Array.new(N + 1, 0)
+# i = 0
+# while true
+#   record << A[i]
+#   seen[i] += 1
+#   i = A[i]
+#   # puts("i: #{i} seen: #{seen} record: #{record}")
+#   if seen[i] > 0
+#     while record[0] != i
+#       if record[0] == i
+#         break
+#       end
+
+#       if $K == 0
+#         break
+#       else
+#         $K -= 1
+#         record.shift
+#       end
+#     end
+
+#     break
+#   end
+# end
+
+# # p($K)
+# # p(record)
+# # p($K % (record.length - 1))
+# p(record[$K % (record.length - 1)])
+
+# ダブリングによる解法
 N, $K = gets.split.map(&:to_i)
-A = gets.split.map(&:to_i)
-A.unshift(1)
-record = []
-seen = Array.new(N + 1, 0)
-i = 0
-while true
-  record << A[i]
-  seen[i] += 1
-  i = A[i]
-  # puts("i: #{i} seen: #{seen} record: #{record}")
-  if seen[i] > 0
-    while record[0] != i
-      if record[0] == i
-        break
-      end
+A = gets.split.map { |a| a.to_i - 1 }
+dp = Array.new(61) { Array.new(N, 0) }
+N.times do |i|
+  dp[0][i] = A[i]
+end
 
-      if $K == 0
-        break
-      else
-        $K -= 1
-        record.shift
-      end
-    end
-
-    break
+1.upto(60) do |i|
+  N.times do |j|
+    dp[i][j] = dp[i - 1][dp[i - 1][j]]
   end
 end
 
-# p($K)
-# p(record)
-# p($K % (record.length - 1))
-p(record[$K % (record.length - 1)])
+ans = 0
+i = 0
+while $K > 0
+  if $K & 1 > 0
+    ans = dp[i][ans]
+  end
+
+  $K >>= 1
+  i += 1
+end
+
+puts(ans + 1)
---
+++ atcoder/trainings/tessoku_book_ed.rb
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ed
+N, K = gets.split.map(&:to_i)
+N.times do |n|
+  n += 1
+end
