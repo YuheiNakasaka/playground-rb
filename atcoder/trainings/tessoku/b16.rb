7aea7bdbf522164b753bdecded94ec5113ec6fc5
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-05-31 16:04:57 +0900

    tessoku b16

---
+++ atcoder/trainings/tessoku/b16.rb
@@ -0,0 +1,22 @@
+# https://atcoder.jp/contests/tessoku-book/tasks/dp_a
+class Solver
+  def initialize
+  end
+
+  def solve
+    n = gets.to_i
+    h = gets.split.map(&:to_i)
+    dp = Array.new(n, 10 ** 5)
+    dp[0] = 0
+    n.times do |i|
+      dp[i] = dp[i - 1] + (h[i - 1] - h[i]).abs if i - 1 >= 0
+      if i - 2 >= 0
+        dp[i] = [dp[i], dp[i - 2] + (h[i - 2] - h[i]).abs].min
+      end
+    end
+
+    puts(dp[-1])
+  end
+end
+
+Solver.new.solve
